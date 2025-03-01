e5ee2445c44b5970223b6f76cece8efd34b22623
DevarshMkwn <devarshmkwn@gmail.com>
2025-03-01 10:30:09 +0530

    init the database

---
+++ src/auth/init.sql
@@ -0,0 +1,24 @@
+-- Step 1: Create a new MySQL user
+CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'auth_pass';
+
+-- Step 2: Create a new database for the Auth Service
+CREATE DATABASE auth_db;
+
+-- Step 3: Grant privileges to the user
+GRANT ALL PRIVILEGES ON auth_db.* TO 'auth_user'@'localhost';
+
+-- Step 4: Switch to the database
+USE auth_db;
+
+-- Step 5: Create the users table
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (name, email, password) VALUES ('johndoe@example.com', 'SecurePassword123');
+
+-- Step 6: Apply changes
+FLUSH PRIVILEGES;
--- src/auth/server.py
+++ src/auth/server.py
@@ -1,6 +1,27 @@
 import jwt, datetime, os
 from flask import Flask, request
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+
+load_dotenv()
 
 server = Flask(__name__)
 mysql = MySQL(server)
+
+# Access MySQL-related environment variables
+server.config["MYSQL_USER"] = os.getenv('MYSQL_USER')
+server.config["MYSQL_PASSWORD"] = os.getenv('MYSQL_PASSWORD')
+server.config["MYSQL_DB"] = os.getenv('MYSQL_DB')
+server.config["MYSQL_PORT"] = os.getenv('MYSQL_PORT')
+
+
+@server.route('/')
+def index():
+    return 'MySQL configuration loaded successfully!'
+
+@server.route("/login",methods=["POST"])
+def login():
+
+
+if __name__ == '__main__':
+    server.run(debug=True)
